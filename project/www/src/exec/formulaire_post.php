177445b9cca590f7434d1695c34525bb3d4e3c11
Ludovic NAULOT <pctronique@free.fr>
2022-04-20 16:57:30 +0200

    formulaire post sans balise html

--- project/www/src/exec/formulaire_post.php
+++ project/www/src/exec/formulaire_post.php
@@ -10,10 +10,10 @@ if(!empty($_POST)) {
     $res = $sgbd->prepare("INSERT INTO messages(Nom, Prenom, Email, Message) VALUES ".
     "(:Nom,:Prenom,:Email,:Message)");
     $res->execute([
-        ":Nom" => trim(stripslashes(htmlspecialchars($_POST['name']))),
-        ":Prenom" => trim(stripslashes(htmlspecialchars($_POST['first_name']))),
-        ":Email" => trim(stripslashes(htmlspecialchars($_POST['mail']))),
-        ":Message" => trim(stripslashes(htmlspecialchars($_POST['user_text']))),
+        ":Nom" => trim(stripslashes(strip_tags($_POST['name']))),
+        ":Prenom" => trim(stripslashes(strip_tags($_POST['first_name']))),
+        ":Email" => trim(stripslashes(strip_tags($_POST['mail']))),
+        ":Message" => trim(stripslashes(strip_tags($_POST['user_text']))),
     ]);
     
     echo "le message a été envoyé.";
