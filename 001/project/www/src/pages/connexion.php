4b5333436eb5d9e258a0a1c5ce443c3b32a0762c
pctronique <pctronique@free.fr>
2024-05-17 09:01:12 +0200

    del folder 001

--- 001/project/www/src/pages/connexion.php
+++
@@ -1,7 +0,0 @@
-<section id="connexion">
-    <form action="./?pg=conn" method="post">
-        <label>Login</label><input type="text" name="login" id="login" />
-        <label>Password</label><input type="password" name="password" id="password" autocomplete="on" />
-        <button type="sumit" class="button_bleu two_column_button">Validation</button>
-    </form>
-</section>
